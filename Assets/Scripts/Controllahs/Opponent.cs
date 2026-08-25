13928b13b6aebb9b288836621a0cf9674c81caa7
JakeSanderson350 <jake.sanderson@mymail.champlain.edu>
2026-08-24 21:33:22 -0400

    luh organization

--- Assets/Scripts/Controllahs/Opponent.cs
+++ Assets/Scripts/Controllahs/Opponent.cs
@@ -1,4 +1,3 @@
-using UnityEngine;
 
 // Opponent decision logic prolly
 public class Opponent : HandController
--- Assets/Scripts/Controllahs/Player.cs
+++ Assets/Scripts/Controllahs/Player.cs
@@ -1,4 +1,3 @@
-using UnityEngine;
 
 // Player controls
 public class Player : HandController
